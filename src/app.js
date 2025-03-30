dea6432ce142e8a82c7940532fce1c67953a1d89
bit-frame <linuskang09@gmail.com>
2025-03-30 09:53:21 +0000

    added api file, rename, update app.js logging

--- src/app.js
+++ src/app.js
@@ -18,7 +18,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 const staticRoutes = require("./routes/static");
+const apiRoutes = require("./routes/api");
 app.use(staticRoutes);
+app.use(apiRoutes);
 
 app.use((req, res, next) => {
     res.status(404).send('404 Not Found');
@@ -26,5 +28,5 @@ app.use((req, res, next) => {
 
 app.listen(process.env.port, "0.0.0.0", () => {
     log.info(`Thanks for using Blogger! Made with ❤️ by bit-frame`);
-    log.info(`Server Started | Version ${v} | Access at 0.0.0.0:${process.env.port}`);
+    log.info(`Server Version ${v} | Access at 0.0.0.0:${process.env.port}`);
 });
